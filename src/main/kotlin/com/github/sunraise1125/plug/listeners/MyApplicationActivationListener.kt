4dc96363b2d16f56c6b289db355ea9efa3f0dffd
GitHub Action <action@github.com>
2024-06-29 09:08:29 +0000

    Template cleanup

--- src/main/kotlin/com/github/sunraise1125/plug/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/sunraise1125/plug/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.sunraise1125.plug.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
